e10436848e1aa44c03e4bf9be44f0af30de98397
DaniGarcia231 <danigq26@gmail.com>
2020-04-08 20:20:21 -0500

    Cree la clase Topic

---
+++ app/src/main/java/com/example/proyectofinal/Topic.java
@@ -0,0 +1,18 @@
+package com.example.proyectofinal;
+
+public class Topic {
+
+    private int ID;
+
+    public Topic(int ID){
+        this.ID = ID;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+}
