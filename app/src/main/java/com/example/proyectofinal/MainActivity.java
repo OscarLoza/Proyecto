ef50dbb95aa0233819bd4db3dde66573c24b3514
DaniGarcia231 <danigq26@gmail.com>
2020-04-08 20:10:30 -0500

    Nuevo todo

--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -10,10 +10,15 @@ public class MainActivity extends AppCompatActivity {
     private int nuevo4;
 
     public String myStr;
+    public int num1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public String helloWorld(){
+        return "Hello Worl";
     }
 }
