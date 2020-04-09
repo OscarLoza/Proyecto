796452360e84bbd510c7007b5b99f52bb095adda
oscar loza <loza.ortega.oscar@gmail.com>
2020-04-08 19:13:32 -0500

    cambio variable

--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int nuevo; 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
